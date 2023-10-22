17340f2880692c9eac95de9b0477f3315d1569f1
Whatsuup <alexanderalbers01@gmail.com>
2023-10-22 11:56:51 +0200

    added a bit error handling / through passing

--- src/error.rs
+++ src/error.rs
@@ -1,11 +1,31 @@
+use serde_json::Number;
+use thiserror::Error;
+
+pub type Result<T> = std::result::Result<T, TokenGenerationError>;
+
 /// This error contains all predictable types of failures
 /// that can occur during the token generation/signing and requesting.
+#[derive(Debug, Error)]
 pub enum TokenGenerationError {
-    // TODO define types
-
-    // Genration/Creation and other
-
-    // RSA SHA256 / Crypto Errors
-
-    // Reqwest errors
+    /// Invalid lifetime
+    #[error("The provided lifetime '{0}' is out of range 30..3600.")]
+    InvalidLifetime(i64),
+    /// JsonWebToken library error
+    #[error("JsonWebTokenError occurred.")]
+    JsonWebTokenError {
+        #[from]
+        source: jsonwebtoken::errors::Error,
+    },
+    /// Reqwest library error
+    #[error("ReqwestError occurred.")]
+    ReqwestError {
+        #[from]
+        source: reqwest::Error,
+    },
+    /// Serde library error
+    #[error("SerdeError occurred.")]
+    SerdeError {
+        #[from]
+        source: serde_json::Error,
+    },
 }
