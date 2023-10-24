a3adc0d6409e3ad84960e90971503b16ac284c20
Whatsuup <alexanderalbers01@gmail.com>
2023-10-24 13:28:00 +0200

    renaming and structural changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use crate::error::Result;
-use crate::json_structs::{Claims, GoogleBearerResponseJson, JsonWebToken};
+use crate::json_structs::{Claims, GoogleBearerResponseJson, ServiceAccountInfoJson};
 
 use crate::error::TokenGenerationError::InvalidLifetime;
 use jsonwebtoken::{Algorithm, EncodingKey, Header};
@@ -7,18 +7,24 @@ use jsonwebtoken::{Algorithm, EncodingKey, Header};
 mod error;
 mod json_structs;
 
-struct Jwt {
+static GRANT_TYPE: &str = "urn:ietf:params:oauth:grant-type:jwt-bearer";
+static CONTENT_TYPE: &str = "application/x-www-form-urlencoded";
+
+struct AuthConfig {
     header: Header,
-    claims: Claims,
+    iss: String,
+    scope: String,
+    aud: String,
     private_key: String,
 }
 
-impl Jwt {
-    /// This function generates an jwt with the provided information. A jwt is used to request auth_tokens.
+impl AuthConfig {
+    /// This function generates an auth configuration with the provided information. A this config is used to request auth_tokens.
     /// This function generates only tokens with the RS256 encryption like the google jwt authentication service does.
     /// # Params
-    /// **json_jwt_file_str: String**<br>
-    /// Each google service account has an jwt json file that can be downloaded in the google console.
+    /// **service_account_json_str: String**<br>
+    /// Each google service account has a json file that can be downloaded in the google console during the key generation.
+    /// This json file cannot be downloaded twice! A new key must be generated, if the file gets lost!
     /// The content of this file needs to be provided by this param as string.
     ///
     /// **usage: String**<br>
@@ -34,16 +40,15 @@ impl Jwt {
     /// See [`TokenGenerationError`] for a more detailed answer.
     /// # Returns
     /// The above mentioned jwt as String.
-    pub fn build(json_jwt_file_str: String, usage: String, lifetime: i64) -> Result<Self> {
+    pub fn build(service_account_json_str: String, usage: String) -> Result<Self> {
         //Todo make usage to Custom(String)
-        if !(29..3601).contains(&lifetime) {
-            return Err(InvalidLifetime(lifetime));
-        }
-        let jwt_json: JsonWebToken = serde_json::from_str(&json_jwt_file_str)?;
+        let account_info: ServiceAccountInfoJson = serde_json::from_str(&service_account_json_str)?;
         Ok(Self {
-            header: create_header(jwt_json.client_id),
-            claims: create_claims(jwt_json.client_email, usage, jwt_json.token_uri, lifetime),
-            private_key: jwt_json.private_key,
+            header: Header::new(Algorithm::RS256),
+            iss: account_info.client_email,
+            scope: usage,
+            aud: account_info.token_uri,
+            private_key: account_info.private_key,
         })
     }
 
@@ -53,16 +58,21 @@ impl Jwt {
     /// See [`TokenGenerationError`] for a more detailed answer.
     /// # Returns
     /// The above mentioned auth_token as String.
-    pub async fn request_auth_token(&self) -> Result<String> {
-        let grant_type = "urn:ietf:params:oauth:grant-type:jwt-bearer"; // lazy static?
-        let assertion = self.sign()?;
-        let params = format!("grant_type={grant_type}&assertion={assertion}");
+    pub async fn request_auth_token(&self, lifetime: i64) -> Result<String> {
+        if !(29..3601).contains(&lifetime) {
+            return Err(InvalidLifetime(lifetime));
+        }
+
+        // TODO add token buffer with lifetime check to minimize auth_token requests
+        // <--
+
+        let claims = Claims::new(&self.iss, &self.scope, &self.aud, lifetime);
+        let assertion = self.sign(claims)?;
+
+        let params = format!("grant_type={GRANT_TYPE}&assertion={assertion}");
         let resp = reqwest::Client::new()
-            .post(&self.claims.aud)
-            .header(
-                reqwest::header::CONTENT_TYPE,
-                "application/x-www-form-urlencoded",
-            )
+            .post(&self.aud)
+            .header(reqwest::header::CONTENT_TYPE, CONTENT_TYPE)
             .body(params)
             .send()
             .await?
@@ -71,30 +81,14 @@ impl Jwt {
         Ok(resp.access_token)
     }
 
-    fn sign(&self) -> Result<String> {
-        let key = EncodingKey::from_rsa_pem(self.private_key.as_bytes())?;
+    fn sign(&self, claims: Claims) -> Result<String> {
+        let key = EncodingKey::from_rsa_pem(&self.private_key.as_bytes())?;
         Ok(jsonwebtoken::encode::<Claims>(
             &self.header,
-            &self.claims,
+            &claims,
             &key,
         )?)
     }
-}
-
-fn create_claims(client_email: String, usage: String, token_uri: String, lifetime: i64) -> Claims {
-    let now = chrono::offset::Utc::now().timestamp();
-    let expire = now + lifetime;
-    Claims {
-        iss: client_email,
-        scope: usage,
-        aud: token_uri,
-        exp: expire,
-        iat: now,
-    }
-}
-
-fn create_header(_client_id: String) -> Header {
-    Header::new(Algorithm::RS256) // add client_id? (not necessary!)
 }
 
 #[cfg(test)]
@@ -105,8 +99,8 @@ mod tests {
 
     #[tokio::test]
     async fn test_generate_auth_token() {
-        let jwt = get_valid_jwt();
-        let token = jwt.request_auth_token().await;
+        let config = get_valid_jwt();
+        let token = config.request_auth_token(3600).await;
         assert!(token.is_ok());
         println!("{}", token.unwrap());
     }
@@ -114,12 +108,11 @@ mod tests {
     #[tokio::test]
     async fn test_generate_auth_token_wrong_json() {
         let contents = fs::read_to_string("tests/invalid-client.json").unwrap();
-        let jwt = Jwt::build(
+        let config = AuthConfig::build(
             contents,
-            String::from("https://www.googleapis.com/auth/cloud-vision"),
-            3600,
+            String::from("https://www.googleapis.com/auth/cloud-vision")
         );
-        assert!(jwt.is_err());
+        assert!(config.is_err());
     }
 
     //#[tokio::test]
@@ -142,44 +135,18 @@ mod tests {
         todo!()
     }
 
-    #[test]
-    fn test_header() {
-        assert_eq!(create_header(String::new()), Header::new(Algorithm::RS256))
-    }
-
-    #[test]
-    fn test_claims() {
-        let mail = String::from("any");
-        let usage = String::from("any");
-        let token_uri = String::from("any");
-        let lifetime = 20;
-        let now = chrono::offset::Utc::now().timestamp();
-        let expire = now + lifetime;
-
-        let assumption = Claims {
-            iss: mail.clone(),
-            scope: usage.clone(),
-            aud: token_uri.clone(),
-            exp: expire,
-            iat: now,
-        };
-        assert_eq!(create_claims(mail, usage, token_uri, lifetime), assumption)
-    }
-
-    fn get_valid_jwt() -> Jwt {
-        Jwt::build(
+    fn get_valid_jwt() -> AuthConfig {
+        AuthConfig::build(
             fs::read_to_string("tests/test-client.json").unwrap(),
-            String::from("https://www.googleapis.com/auth/cloud-vision"),
-            3600,
+            String::from("https://www.googleapis.com/auth/cloud-vision")
         )
         .unwrap()
     }
 
-    fn get_valid_jwt_with_invalid_values() -> Jwt {
-        Jwt::build(
+    fn get_valid_jwt_with_invalid_values() -> AuthConfig {
+        AuthConfig::build(
             fs::read_to_string("tests/invalid-value-client.json").unwrap(),
-            String::from("https://www.googleapis.com/auth/cloud-vision"),
-            3600,
+            String::from("https://www.googleapis.com/auth/cloud-vision")
         )
         .unwrap()
     }
