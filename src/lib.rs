a828d662848426e35112ddebea30169dc4096eac
Whatsuup <alexanderalbers01@gmail.com>
2023-11-15 18:53:42 +0100

    clippy and formatting fixes

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 #![forbid(unsafe_code)]
 #![warn(
-missing_docs,
-unreachable_pub,
-unused_crate_dependencies,
-clippy::pedantic,
-clippy::nursery,
-clippy::unwrap_used,
-clippy::dbg_macro,
-clippy::todo
+    missing_docs,
+    unreachable_pub,
+    unused_crate_dependencies,
+    clippy::pedantic,
+    clippy::nursery,
+    clippy::unwrap_used,
+    clippy::dbg_macro,
+    clippy::todo
 )]
 #![allow(clippy::module_name_repetitions)]
 
@@ -38,30 +38,30 @@ pub struct AuthConfig {
 }
 
 impl AuthConfig {
-    /// This function generates an auth configuration with the provided information. A this config is used to request auth_tokens.
+    /// This function generates an auth configuration with the provided information. A this config is used to request `auth_tokens`.
     /// This function generates only tokens with the RS256 encryption like the google jwt authentication service does.
     /// # Params
-    /// **service_account_json_str: String**<br>
+    /// **`service_account_json_str`: String**<br>
     /// Each google service account has a json file that can be downloaded in the google console during the key generation.
     /// This json file cannot be downloaded twice! A new key must be generated, if the file gets lost!
     /// The content of this file needs to be provided by this param as string.
     ///
-    /// **usage: String**<br>
+    /// **`usage`: String**<br>
     /// Each google api request requires individual permissions to be executed.
     /// Beside the service account permission a usage or a scope should be provided.
     /// See here for more information: URL.
     ///
-    /// **lifetime: u16**<br>
-    /// An auth_token has a limited lifetime to am maximum of 3600 seconds.
+    /// **`lifetime`: u16**<br>
+    /// An `auth_token` has a limited lifetime to am maximum of 3600 seconds.
     /// This value should be between 30 and 3600 Seconds.
     /// Inputs out of this ranged will not be accepted.
     /// # Errors
     /// See [`TokenGenerationError`] for a more detailed answer.
     /// # Returns
     /// The above mentioned jwt as String.
-    pub fn build(service_account_json_str: String, usage: String) -> Result<Self> {
+    pub fn build(service_account_json_str: &str, usage: String) -> Result<Self> {
         //Todo make usage to Custom(String)
-        let account_info: ServiceAccountInfoJson = serde_json::from_str(&service_account_json_str)?;
+        let account_info: ServiceAccountInfoJson = serde_json::from_str(service_account_json_str)?;
         Ok(Self {
             header: Header::new(Algorithm::RS256),
             iss: account_info.client_email,
@@ -71,12 +71,12 @@ impl AuthConfig {
         })
     }
 
-    /// With the provided jwt token, an authentication token (short: auth_token) will be requested from google.
-    /// This auth_token will be returned and is used for requesting several google api services.
+    /// With the provided jwt token, an authentication token (short: `auth_token`) will be requested from google.
+    /// This `auth_token` will be returned and is used for requesting several google api services.
     /// # Errors
     /// See [`TokenGenerationError`] for a more detailed answer.
     /// # Returns
-    /// The above mentioned auth_token as String.
+    /// The above mentioned `auth_token` as String.
     pub async fn generate_auth_token(&self, lifetime: i64) -> Result<String> {
         if !(30..=3600).contains(&lifetime) {
             return Err(InvalidLifetime(lifetime));
@@ -134,7 +134,7 @@ mod tests {
         // The following config depends on an deleted service account key.
         // It is no longer possible to create tokens with this info.
         let invalid_config = AuthConfig::build(
-            fs::read_to_string("tests/test-client-old.json").unwrap(),
+            &fs::read_to_string("tests/test-client-old.json").unwrap(),
             String::from("https://www.googleapis.com/auth/cloud-vision"),
         )
         .unwrap();
@@ -146,7 +146,7 @@ mod tests {
     #[tokio::test]
     async fn test_generate_auth_token_wrong_json() {
         let config = AuthConfig::build(
-            fs::read_to_string("tests/invalid-client.json").unwrap(),
+            &fs::read_to_string("tests/invalid-client.json").unwrap(),
             String::from("https://www.googleapis.com/auth/cloud-vision"),
         );
         assert!(config.is_err());
@@ -179,6 +179,10 @@ mod tests {
     }
 
     fn get_valid_config(usage: String) -> AuthConfig {
-        AuthConfig::build(fs::read_to_string("tests/test-client.json").unwrap(), usage).unwrap()
+        AuthConfig::build(
+            &fs::read_to_string("tests/test-client.json").unwrap(),
+            usage,
+        )
+        .unwrap()
     }
 }
